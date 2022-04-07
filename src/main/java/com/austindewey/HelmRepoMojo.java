b2ed14bbb60098a76b23e056be3d0682a80d2ebc
Austin Dewey <deweya964@gmail.com>
2022-04-07 14:16:17 -0400

    adding init plugin

---
+++ src/main/java/com/austindewey/HelmRepoMojo.java
@@ -0,0 +1,17 @@
+package com.austindewey;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "helm-repo", defaultPhase = LifecyclePhase.COMPILE)
+public class HelmRepoMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("This is a test message");
+	}
+
+}
