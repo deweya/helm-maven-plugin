78b72ec5d047989b00bc9776593d563ff6bd00aa
Austin Dewey <deweya964@gmail.com>
2022-04-09 06:35:42 -0400

    able to invoke helm cli

--- src/main/java/com/austindewey/HelmUpgradeMojo.java
+++ src/main/java/com/austindewey/HelmUpgradeMojo.java
@@ -1,5 +1,9 @@
 package com.austindewey;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -11,7 +15,22 @@ public class HelmUpgradeMojo extends AbstractMojo {
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("This is a test message");
+		Runtime rt = Runtime.getRuntime();
+		try {
+			Process proc = rt.exec("helm version");
+			BufferedReader stdin = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+			BufferedReader stderr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+			
+			String s;
+			while ((s = stdin.readLine()) != null) {
+				System.out.println(s);
+			}
+			
+			while ((s = stderr.readLine()) != null) {
+				System.out.println(s);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
