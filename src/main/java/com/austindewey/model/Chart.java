7448840ff2758403ef4ea6cb069a691182dd446a
Austin Dewey <deweya964@gmail.com>
2022-04-09 07:49:07 -0400

    using lombok

--- src/main/java/com/austindewey/model/Chart.java
+++ src/main/java/com/austindewey/model/Chart.java
@@ -1,38 +1,16 @@
 package com.austindewey.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@SuppressWarnings("unused")
 public class Chart {
 	
 	private String name;
 	private String version;
 	private Repository repository;
-	
-	public Chart(String name, String version, Repository repository) {
-		this.name = name;
-		this.version = version;
-		this.repository = repository;
-	}
-	
-	public Chart() {}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
-	
-	public void setVersion(String version) {
-		this.version = version;
-	}
-	public String getVersion() {
-		return version;
-	}
-	
-	public void setRepository(Repository repository) {
-		this.repository = repository;
-	}
-	public Repository getRepository() {
-		return repository;
-	}
 
 }
--- src/main/java/com/austindewey/model/Repository.java
+++ src/main/java/com/austindewey/model/Repository.java
@@ -1,28 +1,15 @@
 package com.austindewey.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@SuppressWarnings("unused")
 public class Repository {
 
 	private String name;
 	private String url;
 	
-	public Repository(String name, String url) {
-		this.name = name;
-		this.url = url;
-	}
-	
-	public Repository() {}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
-	
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getUrl() {
-		return url;
-	}
 }
